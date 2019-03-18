d3bca7608f4d9d79409080c52018e8a1806e4fee
Alexey Pechorin <Alexey.p@taboola.com>
2019-03-18 16:22:55 +0200

    Add script to run both data synthesis and training

---
+++ run_data_synthesis_and_train.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+cd data_preperation;
+python 1_prepare_orig_images.py;
+python 2_prepare_synth_images.py;
+python 3_create_class_dict.py;
+
+cd ..;
+
+CUDA_VISIBLE_DEVICES=0 train.py --do-test-vat True --vat-epsilon 0.5 --vat-xi 1e-6 --vat-sign True --vat-ratio 10. \
+--output-dir '../Output/transductive_vat' --do-lr-step True
