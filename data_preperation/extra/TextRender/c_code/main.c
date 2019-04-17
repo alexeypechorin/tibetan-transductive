def099623ed4427ab611e33675954bbed9243c4b
Alexey Pechorin <Alexey.p@taboola.com>
2019-04-18 01:43:19 +0300

    Clearer error and debug messages in text rendering tool

--- data_preperation/extra/TextRender/c_code/main.c
+++ data_preperation/extra/TextRender/c_code/main.c
@@ -9,8 +9,8 @@
 int main (int argc, char ** argv)
 {
     if (argc != 9){
-        printf("Error: must recieve two parameters - <text to print> <output path> <font directory> <font family>"
-        "<font weight> <font consetration>\n");
+        printf("Error: must receive 8 parameters - <text to print> <output path> <fonts directory> <font family> <font weight index 0-5> <font stretch 0-8> <letter spacing 0-3> <font size 0-3>");
+        printf("Received %d parameters!\n", argc - 1);
         return -1;
     }
     const char * const FONT_WEIGHTS[] = { "ultralight", "light", "normal", "bold", "ultrabold", "heavy" };
@@ -18,27 +18,33 @@ int main (int argc, char ** argv)
   "normal", "semiexpanded", "expanded", "extraexpanded", "ultraexpanded" };
     const char * const LETTER_SPACING[] = { "0", "1000", "2000" };
     const char * const FONT_SIZE[] = { "10000", "150000", "20000", "25000" };
-    const char *font_family = argv[4];
 
-    char *out_path = malloc(sizeof(char) * (strlen(argv[2]) + 1));
-    strcpy(out_path, argv[2]);
+    const char *text_to_print = argv[1];
+    char *output_path = malloc(sizeof(char) * (strlen(argv[2]) + 1));
+    strcpy(output_path, argv[2]);
+
+    char *fonts_directory = argv[3];
+    const char *font_family = argv[4];
 
     const char * font_weight = FONT_WEIGHTS[atoi(argv[5])];
     const char * font_stretch = FONT_STRETCHES[atoi(argv[6])];
     const char * letter_spacing = LETTER_SPACING[atoi(argv[7])];
     const char * font_size = FONT_SIZE[atoi(argv[8])];
 
+    printf("Using parameters:\nText to print=%s\nOutput path=%s\nFonts directory=%s\nFont family=%s\nFont weight=%s\nFont stretch=%s\nLetter spacing=%s\nFont size=%s\n",
+    text_to_print, output_path, fonts_directory, font_family, font_weight, font_stretch, letter_spacing, font_size);
 
-    char *murkup_pattern = "<span foreground='black' background='white' font_family='%s' font_size='%s' font_weight='%s' font_stretch='%s' letter_spacing=%s></span>";
-    //printf("%s\n",murkup_pattern);
-    char *murkup_text = malloc(sizeof(char) * (strlen(murkup_pattern) + strlen(font_weight) + strlen(font_stretch) \
-    + strlen(font_family) + strlen(argv[1]) + strlen(letter_spacing) + 10 + strlen(font_size) )) ;
-    //printf("%s\n",murkup_pattern);
-    sprintf(murkup_text, "<span foreground='black' background='white' font_family='%s' font_size='%s' font_weight='%s' font_stretch='%s' letter_spacing='%s'>%s</span>",
-     font_family, font_size, font_weight, font_stretch, letter_spacing,argv[1]);
-    create_text_image(out_path, murkup_text, argv[3]);
-    //free(out_path);
-    //free(murkup_text);
+    char *markup_pattern = "<span foreground='black' background='white' font_family='%s' font_size='%s' font_weight='%s' font_stretch='%s' letter_spacing=%s></span>";
+    //printf("%s\n",markup_pattern);
+    char *markup_text = malloc(sizeof(char) * (strlen(markup_pattern) + strlen(font_weight) + strlen(font_stretch) \
+    + strlen(font_family) + strlen(text_to_print) + strlen(letter_spacing) + 10 + strlen(font_size) )) ;
+    //printf("%s\n",markup_pattern);
+    sprintf(markup_text, "<span foreground='black' background='white' font_family='%s' font_size='%s' font_weight='%s' font_stretch='%s' letter_spacing='%s'>%s</span>",
+     font_family, font_size, font_weight, font_stretch, letter_spacing, text_to_print);
+    printf("Using markup text:\n%s\n", markup_text);
+    create_text_image(output_path, markup_text, fonts_directory);
+    free(output_path);
+    free(markup_text);
     return 0;
      
 }
