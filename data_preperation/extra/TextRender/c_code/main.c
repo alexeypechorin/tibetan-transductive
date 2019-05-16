10ad29bb8e264872f7e13bffdd73673e42c516de
Alexey Pechorin <Alexey.p@taboola.com>
2019-05-16 18:08:22 +0300

    Add debug parameter for printing out text rendering parameters

--- data_preperation/extra/TextRender/c_code/main.c
+++ data_preperation/extra/TextRender/c_code/main.c
@@ -8,11 +8,12 @@
 
 int main (int argc, char ** argv)
 {
-    if (argc != 9){
-        printf("Error: must receive 8 parameters - <text to print> <output path> <fonts directory> <font family> <font weight index 0-5> <font stretch 0-8> <letter spacing 0-3> <font size 0-3>");
+    if (argc < 9){
+        printf("Error: must receive at least 8 parameters - <text to print> <output path> <fonts directory> <font family> <font weight index 0-5> <font stretch 0-8> <letter spacing 0-3> <font size 0-3> <DEBUG(optional)>");
         printf("Received %d parameters!\n", argc - 1);
         return -1;
     }
+    char DEBUG = 1;
     const char * const FONT_WEIGHTS[] = { "ultralight", "light", "normal", "bold", "ultrabold", "heavy" };
     const char * const FONT_STRETCHES[] = { "ultracondensed", "extracondensed", "condensed", "semicondensed", \
   "normal", "semiexpanded", "expanded", "extraexpanded", "ultraexpanded" };
@@ -31,8 +32,14 @@ int main (int argc, char ** argv)
     const char * letter_spacing = LETTER_SPACING[atoi(argv[7])];
     const char * font_size = FONT_SIZE[atoi(argv[8])];
 
-    printf("Using parameters:\nText to print=%s\nOutput path=%s\nFonts directory=%s\nFont family=%s\nFont weight=%s\nFont stretch=%s\nLetter spacing=%s\nFont size=%s\n",
-    text_to_print, output_path, fonts_directory, font_family, font_weight, font_stretch, letter_spacing, font_size);
+    if (argc == 10) {
+        DEBUG = atoi(argv[9]);
+    }
+
+    if (DEBUG == 1) {
+        printf("Using parameters:\nText to print=%s\nOutput path=%s\nFonts directory=%s\nFont family=%s\nFont weight=%s\nFont stretch=%s\nLetter spacing=%s\nFont size=%s\n",
+        text_to_print, output_path, fonts_directory, font_family, font_weight, font_stretch, letter_spacing, font_size);
+    }
 
     char *markup_pattern = "<span foreground='black' background='white' font_family='%s' font_size='%s' font_weight='%s' font_stretch='%s' letter_spacing=%s></span>";
     //printf("%s\n",markup_pattern);
@@ -41,7 +48,9 @@ int main (int argc, char ** argv)
     //printf("%s\n",markup_pattern);
     sprintf(markup_text, "<span foreground='black' background='white' font_family='%s' font_size='%s' font_weight='%s' font_stretch='%s' letter_spacing='%s'>%s</span>",
      font_family, font_size, font_weight, font_stretch, letter_spacing, text_to_print);
-    printf("Using markup text:\n%s\n", markup_text);
+    if (DEBUG == 1) {
+        printf("Using markup text:\n%s\n", markup_text);
+    }
     create_text_image(output_path, markup_text, fonts_directory);
     free(output_path);
     free(markup_text);
