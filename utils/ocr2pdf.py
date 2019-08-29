8638ae0d97112b0f652f80cd3895539203f21816
Alexey Pechorin <alexey.p@taboola.com>
2019-08-29 16:39:34 +0300

    Put OCR original and transcriptions side by side in the resulting PDF file

--- utils/ocr2pdf.py
+++ utils/ocr2pdf.py
@@ -1,35 +1,55 @@
-from PIL import Image, ImageDraw
+# -*- coding: utf-8 -*-
+from argparse import ArgumentParser
+from PIL import Image, ImageDraw, ImageFont
 import os
 
 if __name__ == '__main__':
-    image_list_filename="/Users/alexey.p/thesis/tmp/tibetan_ocr_results/test_val_orig_0_im.txt"
-    pred_filename="/Users/alexey.p/thesis/tmp/tibetan_ocr_results/test_val_orig_0_pred_no_stopchars.txt"
-    sorted_pred_filename="/Users/alexey.p/thesis/tmp/tibetan_ocr_results/test_val_orig_0_pred_no_stopchars_sorted.txt"
-    images_base_dir = '/Users/alexey.p/thesis/tmp/tibetan_ocr_results/LineImages/'
-    output_image_filename = "1k_sorted.pdf"
+    parser = ArgumentParser()
+    parser.add_argument('-i', '--image_list_filename', type=str,
+                        default="/Users/alexey.p/thesis/tmp/tibetan_ocr_results/test_val_orig_0_im.txt")
+    parser.add_argument('-p', '--pred_filename', type=str,
+                        default="/Users/alexey.p/thesis/tmp/tibetan_ocr_results/test_val_orig_0_pred_no_stopchars.txt")
+    parser.add_argument('-s', '--sorted_pred_filename', type=str,
+                        default="/Users/alexey.p/thesis/tmp/tibetan_ocr_results/test_val_orig_0_pred_no_stopchars_sorted.txt")
+    parser.add_argument('-b', '--images_base_dir', type=str,
+                        default="/Users/alexey.p/thesis/tmp/tibetan_ocr_results/LineImages/")
+    parser.add_argument('-o', '--output_image_filename', type=str,
+                        default="1k_sorted.pdf")
+    parser.add_argument('-f', '--font', type=str,
+                        default="Qomolangma-Drutsa.ttf")
+    args = parser.parse_args()
 
-    image_list_file = open(image_list_filename,'r')
-    pred_list_file = open(pred_filename,'r')
+    image_list_file = open(args.image_list_filename,'r')
+    pred_list_file = open(args.pred_filename,'r')
     image_names = image_list_file.readlines()
     image_indices = [(int(os.path.basename(im_name).split('_')[0]), int(os.path.basename(im_name).split('_')[2].split('.')[0])) for im_name in image_names]
     preds = pred_list_file.readlines()
     image_list_file.close()
     pred_list_file.close()
 
-    sorted_pred_list_file = open(sorted_pred_filename,'w')
+    sorted_pred_list_file = open(args.sorted_pred_filename,'w')
 
     imagelist = []
     amount = -1
     sorted_pairs = sorted(zip(preds[:], image_names[:], image_indices[:]), key=lambda x: x[2])[:amount]
 
     for pred, im_name, im_index in sorted_pairs:
-        image_filename = images_base_dir + os.path.basename(im_name).rstrip()
+        image_filename = args.images_base_dir + os.path.basename(im_name).rstrip()
         # print(image_filename)
         cover = Image.open(image_filename)
         imagelist.append(cover)
         print(pred)
+
+        pred_img = Image.new('RGB', (cover.size[0] + 100, cover.size[1] + 100), 'white')
+        im_draw = ImageDraw.Draw(pred_img)
+        font_name = args.font
+        font_size = 64
+        font = ImageFont.truetype(font_name, font_size)
+        im_draw.text((400, 20), pred.decode('utf8'), fill=(0, 0, 0), font=font)
+        imagelist.append(pred_img)
+
         sorted_pred_list_file.write(pred)
         # cover.show()
 
     sorted_pred_list_file.close()
-    imagelist[0].save(output_image_filename, "PDF", resolution=100.0, save_all=True, append_images=imagelist[1:])
+    imagelist[0].save(args.output_image_filename, "PDF", resolution=100.0, save_all=True, append_images=imagelist[1:])
